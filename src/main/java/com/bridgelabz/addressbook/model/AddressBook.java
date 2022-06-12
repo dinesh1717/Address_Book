30317fe0223f8d48ed98cd8ed7a917968d03d840
dinesh nangare <dineshnangare1717@gmail.com>
2022-06-12 20:20:44 +0530

    Added: Rest controller added to demonstrate various HTTP method

--- src/main/java/com/bridgelabz/addressbook/model/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/model/AddressBook.java
@@ -1,7 +1,17 @@
 package com.bridgelabz.addressbook.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class AddressBook {
+
+
+    @Id
+    @GeneratedValue
     private Integer id;
+
     private String firstName;
     private String lastName;
     private String email;
@@ -10,15 +20,100 @@ public class AddressBook {
     private String state;
     private Integer zip;
 
+    public AddressBook() {
+        super();
+    }
 
-    public AddressBook(Integer id, String firstName, String lastName, String email, Long phoneNumber, String city, String state, Integer zip) {
+    /**
+     * Created constructor for post address method
+     */
+    public AddressBook(AddressBook addressBook) {
+        super();
+        this.firstName = addressBook.firstName;
+        this.lastName = addressBook.lastName;
+        this.email = addressBook.email;
+        this.phoneNumber = addressBook.phoneNumber;
+        this.city = addressBook.city;
+        this.state = addressBook.state;
+        this.zip = addressBook.zip;
+    }
+
+    /**
+     * Created constructor for update by id method
+     */
+
+    public AddressBook(AddressBook addressBook, Integer id) {
         this.id = id;
+        this.firstName = addressBook.firstName;
+        this.lastName = addressBook.lastName;
+        this.email = addressBook.email;
+        this.phoneNumber = addressBook.phoneNumber;
+        this.city = addressBook.city;
+        this.state = addressBook.state;
+        this.zip = addressBook.zip;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
         this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
         this.state = state;
+    }
+
+    public Integer getZip() {
+        return zip;
+    }
+
+    public void setZip(Integer zip) {
         this.zip = zip;
     }
 }
