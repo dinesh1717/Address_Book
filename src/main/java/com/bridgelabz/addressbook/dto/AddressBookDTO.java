3190c26eecf3cfb27e03135933239af2e82abdf9
dinesh nangare <dineshnangare1717@gmail.com>
2022-06-13 13:14:13 +0530

    Added:service layer to store data

--- src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.addressbook.dto;
 
-public class AddressBookDTO {
+import lombok.Data;
 
+@Data
+public class AddressBookDTO {
     private String firstName;
     private String lastName;
     private String email;
@@ -10,59 +12,7 @@ public class AddressBookDTO {
     private String state;
     private Integer zip;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public Integer getZip() {
-        return zip;
-    }
-
-    public void setZip(Integer zip) {
-        this.zip = zip;
+    public AddressBookDTO() {
+        super();
     }
 }
